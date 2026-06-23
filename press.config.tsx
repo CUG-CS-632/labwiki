922f43b0b1b0a25ea33f54584056a4d3186c2e2b
cug-jfliu <jfliu@cug.edu.cn>
2026-06-23 20:50:06 +0800

    feat: update configuration files and add GitHub Pages workflow

--- press.config.tsx
+++ press.config.tsx
@@ -2,14 +2,14 @@ import { defineConfig } from "fumapress";
 import { fumadocsMdx } from "fumapress/adapters/mdx";
 import { flexsearchPlugin } from "fumapress/plugins/flexsearch";
 import { llmsPlugin } from "fumapress/plugins/llms.txt";
-import { takumiPlugin } from "fumapress/plugins/takumi";
 import { docs } from "./.source/server";
 
 export default defineConfig({
   content: docs.toFumadocsSource(),
   site: {
-    name: "Fumapress",
+    name: "LabWiki",
+    baseUrl: "https://cug-cs-632.github.io/labwiki/",
   },
 })
-  .plugins(flexsearchPlugin(), llmsPlugin(), takumiPlugin())
+  .plugins(flexsearchPlugin(), llmsPlugin())
   .adapters(fumadocsMdx());
